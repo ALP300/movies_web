398f87dcbd0387d5be11c67ad467b1274b9aacb2
ALP300 <aitorpalomino1@gmail.com>
2024-12-10 20:30:03 -0500

    Actualizar proyecto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {dirname,join} from 'path'
 import { fileURLToPath } from 'url'
 import router from './routes/index.js'
+import {Conectar} from './public/services/conexion.js'
 const app= express()
 const __dirname= dirname(fileURLToPath(import.meta.url))
 console.log(join(__dirname, 'views'))
@@ -11,6 +12,8 @@ app.set('view engine', 'ejs')
 app.use(router)
 
 app.use(express.static(join(__dirname,'public')))
+
+Conectar()
 
 app.listen(3000)
 console.log('Server está siendo escuchado en el puerto ',3000)
